86c459a1e04bd8d40c598620f216ee597130abd8
Drosdeck <edsonsbs_2007@yahoo.com.br>
2019-05-09 14:37:41 -0300

    wip

--- acpi/rfkill-acpi/acpirfkill.c
+++ acpi/rfkill-acpi/acpirfkill.c
@@ -2,7 +2,8 @@
 #include <linux/kernel.h>	 
 #include <linux/init.h>	  
 #include <linux/acpi.h>
-#include <linux/acpi_bus.h>
+#include <acpi/acpi_bus.h>
+#include <acpi/acpi_drivers.h>
 #include <linux/rfkill.h>
 #include <linux/types.h>
 MODULE_LICENSE("GPL"); 
@@ -13,24 +14,57 @@ MODULE_DESCRIPTION("bluetooth rfkill to j14kr11");
 
 MODULE_VERSION("1.0"); 
 
+static const struct acpi_device_id  bt_device_ids[] = {
+	{ "INT3420",0},
+        {"",0},
+};
 
-
-
-
-
-static int __init oemwmi_start(void) 
+static int positivo_bt_enable(acpi_handle handle)
 {
-	int status;
+	acpi_status result;
+	acpi_integer status;
+
+
+return 0;
+}
+
+static int positivo_bt_rfkill_add(struct acpi_device *device)
+{
+	return positivo_bt_enable(device->handle);
+
+}
+
+static int positivo_bt_rfkill_remove(struct acpi_device *device)
+{
+	return 0;
+
+}
+
+
+static struct acpi_driver positivo_bt_driver = {
+	.name = "Positivo BT",
+	.class = "Positivo",
+	.ids = bt_device_ids,
+	.ops  = {
+		.add = positivo_bt_rfkill_add,
+                .remove = positivo_bt_rfkill_remove,
+	        },
+	.owner = THIS_MODULE,
+};
+
+static int __init positivo_bt_rfkill_start(void) 
+{
+	int result = 0;
         int err;
 
 	return 0; 
 } 
 
-static void __exit oemwmi_end(void) 
+static void __exit positivo_bt_rfkill_end(void) 
 { 
 	
 } 
 
-module_init(oemwmi_start); 
-module_exit(oemwmi_end); 
+module_init(positivo_bt_rfkill_start); 
+module_exit(positivo_bt_rfkill_end); 
 
